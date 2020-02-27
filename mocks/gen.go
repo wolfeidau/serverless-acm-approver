cc58168c43ea3778a73f3ad73a7992b62e134e7b
Mark Wolfe <mark@wolfe.id.au>
2020-02-27 22:22:10 +1100

    feat(approver) Bit more testing or request and approve operations

--- mocks/gen.go
+++ mocks/gen.go
@@ -3,3 +3,4 @@ package mocks
 //go:generate env GOBIN=$PWD/bin GO111MODULE=on go install github.com/golang/mock/mockgen
 //go:generate $PWD/bin/mockgen -destination=approver.go -package=mocks github.com/wolfeidau/serverless-acm-approver/pkg/approver Certificate
 //go:generate $PWD/bin/mockgen -destination acm.go -package=mocks github.com/aws/aws-sdk-go/service/acm/acmiface ACMAPI
+//go:generate $PWD/bin/mockgen -destination route53.go -package=mocks github.com/aws/aws-sdk-go/service/route53/route53iface Route53API
--- pkg/approver/approver_test.go
+++ pkg/approver/approver_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/route53"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/acm"
 	"github.com/golang/mock/gomock"
@@ -26,4 +28,66 @@ func TestDelete(t *testing.T) {
 
 	err := ca.Delete(context.TODO(), "ghi789")
 	assert.NoError(err)
+}
+
+func TestApprove(t *testing.T) {
+	assert := require.New(t)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	acmapi := mocks.NewMockACMAPI(ctrl)
+	route53api := mocks.NewMockRoute53API(ctrl)
+
+	acmapi.EXPECT().DescribeCertificate(&acm.DescribeCertificateInput{CertificateArn: aws.String("ghi789")}).Return(
+		&acm.DescribeCertificateOutput{Certificate: &acm.CertificateDetail{
+			CertificateArn: aws.String("ghi789"),
+			DomainValidationOptions: []*acm.DomainValidation{
+				&acm.DomainValidation{
+					ResourceRecord: &acm.ResourceRecord{Name: aws.String("_a.1.t.co"), Type: aws.String("CNAME"), Value: aws.String("abc")},
+				},
+			}}}, nil)
+
+	route53api.EXPECT().ChangeResourceRecordSets(gomock.Any()).Return(&route53.ChangeResourceRecordSetsOutput{}, nil)
+	acmapi.EXPECT().WaitUntilCertificateValidatedWithContext(gomock.Any(), &acm.DescribeCertificateInput{CertificateArn: aws.String("ghi789")}, gomock.Any(), gomock.Any()).Return(nil)
+
+	ca := certificateApprover{acm: acmapi, route53: route53api}
+
+	err := ca.Approve(context.TODO(), "ghi789", 300, "a.1.t.co")
+	assert.NoError(err)
+}
+
+func TestCreate(t *testing.T) {
+	assert := require.New(t)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	acmapi := mocks.NewMockACMAPI(ctrl)
+	route53api := mocks.NewMockRoute53API(ctrl)
+
+	acmapi.EXPECT().RequestCertificate(&acm.RequestCertificateInput{
+		DomainName:              aws.String("a.1.t.co"),
+		IdempotencyToken:        aws.String("e99a18c428cb38d5f260853678922e03"),
+		SubjectAlternativeNames: []*string{aws.String("")},
+		ValidationMethod:        aws.String("DNS"),
+	}).Return(&acm.RequestCertificateOutput{CertificateArn: aws.String("ghi789")}, nil)
+
+	acmapi.EXPECT().DescribeCertificate(&acm.DescribeCertificateInput{CertificateArn: aws.String("ghi789")}).Return(
+		&acm.DescribeCertificateOutput{Certificate: &acm.CertificateDetail{
+			CertificateArn: aws.String("ghi789"),
+			DomainValidationOptions: []*acm.DomainValidation{
+				&acm.DomainValidation{
+					ResourceRecord: &acm.ResourceRecord{Name: aws.String("_a.1.t.co"), Type: aws.String("CNAME"), Value: aws.String("abc")},
+				},
+			}}}, nil)
+
+	route53api.EXPECT().ChangeResourceRecordSets(gomock.Any()).Return(&route53.ChangeResourceRecordSetsOutput{}, nil)
+	acmapi.EXPECT().WaitUntilCertificateValidatedWithContext(gomock.Any(), &acm.DescribeCertificateInput{CertificateArn: aws.String("ghi789")}, gomock.Any(), gomock.Any()).Return(nil)
+
+	ca := certificateApprover{acm: acmapi, route53: route53api}
+
+	certificateArn, err := ca.Request(context.TODO(), "abc123", "a.1.t.co", []string{""}, "AZ123")
+	assert.NoError(err)
+	assert.Equal("ghi789", certificateArn)
 }
